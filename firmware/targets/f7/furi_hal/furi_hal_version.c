fbf22f7c257fc2c0257f1411e7a04ce56375f98e
TensorHex <ryan.h.barber@outlook.com>
2023-01-22 23:12:51 -0800

    Device name string

--- firmware/targets/f7/furi_hal/furi_hal_version.c
+++ firmware/targets/f7/furi_hal/furi_hal_version.c
@@ -299,7 +299,7 @@ uint32_t furi_hal_version_get_hw_timestamp() {
 }
 
 const char* furi_hal_version_get_name_ptr() {
-    return *furi_hal_version.name == 0x00 ? NULL : furi_hal_version.name;
+    return "Gh0st";
 }
 
 const char* furi_hal_version_get_device_name_ptr() {
