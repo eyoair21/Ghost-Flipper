e4be4e1757e4cca29e63a8bae5f2cefd3d52b984
TensorHex <4080964+TensorHex@users.noreply.github.com>
2023-01-31 21:44:28 -0800

    firmware update success slideshow

--- applications/settings/about/about.c
+++ applications/settings/about/about.c
@@ -122,7 +122,7 @@ static DialogMessageButton hw_version_screen(DialogsApp* dialogs, DialogMessage*
         furi_hal_version_get_hw_connect(),
         furi_hal_version_get_hw_region_name_otp(),
         furi_hal_region_get_name(),
-        my_name ? my_name : "Unknown");
+        my_name ? my_name : "ghost-010");
 
     furi_string_cat_printf(buffer, "Serial Number:\n");
     const uint8_t* uid = furi_hal_version_uid();
